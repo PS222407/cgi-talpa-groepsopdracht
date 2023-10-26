29e9121e7346b85431987253c48b68b69fcc34e6
jens <jensramakers@gmail.com>
2023-10-26 22:26:30 +0200

    removed tests

--- source/Talpa/Talpa_10_WebApp/Models/TestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Talpa_10_WebApp.Models;
-
-public class TestModel
-{
-    [Required(ErrorMessage = "The Name field is required.")]
-    public string Name { get; set; }
-}
