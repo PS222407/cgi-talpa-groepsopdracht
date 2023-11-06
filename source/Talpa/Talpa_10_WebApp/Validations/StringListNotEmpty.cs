468075c29dbb4ab6dfb5a0dc713f864858ce8cb2
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2023-11-06 15:24:25 +0100

    added translations

--- source/Talpa/Talpa_10_WebApp/Validations/StringListNotEmpty.cs
+++ source/Talpa/Talpa_10_WebApp/Validations/StringListNotEmpty.cs
@@ -1,31 +1,43 @@
 ﻿namespace Talpa_10_WebApp.Validations
 {
+    using Microsoft.AspNetCore.Mvc.DataAnnotations;
+    using Microsoft.Extensions.Localization;
+    using Microsoft.Extensions.Options;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     public class StringListNotEmpty : ValidationAttribute
     {
-        public override bool IsValid(object value)
+        private static IStringLocalizer? _localizer;
+
+        private string GetErrorMessage(ValidationContext validationContext)
+        {
+            return GetLocalizer(validationContext)[ErrorMessage];
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             if (value is List<string> stringList)
             {
-                if (stringList.Count != 0)
+                if (stringList.Count != 0 && stringList.All(item => !string.IsNullOrWhiteSpace(item)))
                 {
-                    foreach (var item in stringList)
-                    {
-                        if (string.IsNullOrWhiteSpace(item))
-                        {
-                            return false;
-                        }
-                    }
-                }
-                else
-                {
-                    return false;
+                    return ValidationResult.Success;
                 }
             }
-            return true; 
+
+            return new ValidationResult(GetErrorMessage(validationContext));
+        }
+
+        private IStringLocalizer GetLocalizer(ValidationContext validationContext)
+        {
+            if (_localizer is null)
+            {
+                IStringLocalizerFactory factory = validationContext.GetRequiredService<IStringLocalizerFactory>();
+                IOptions<MvcDataAnnotationsLocalizationOptions> annotationOptions = validationContext.GetRequiredService<IOptions<MvcDataAnnotationsLocalizationOptions>>();
+                _localizer = annotationOptions.Value.DataAnnotationLocalizerProvider(validationContext.ObjectType, factory);
+            }
+
+            return _localizer;
         }
     }
 
