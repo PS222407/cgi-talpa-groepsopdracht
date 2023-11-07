64fe6ec7015ff6509c932cc2ec023a321537eb71
Jens Ramakers <jensramakers@gmail.com>
2023-11-07 09:40:25 +0100

    show the scoreboard

---
+++ source/Talpa/Talpa_10_WebApp/Controllers/ScoreboardController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogicLayer.Interfaces.Services;
+using BusinessLogicLayer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Talpa_10_WebApp.Controllers;
+
+[Authorize]
+public class ScoreboardController : Controller
+{
+    private readonly IUserService _userService;
+
+    public ScoreboardController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        List<UserScoreboard> userScoreboards = await _userService.GetTopTenUsersWhoOwnTheMostVotedSuggestions(); 
+        
+        return View(userScoreboards);
+    }
+}
