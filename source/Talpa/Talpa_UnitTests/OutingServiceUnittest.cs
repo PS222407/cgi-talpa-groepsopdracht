06331579930bb5d59d60331a3d6bb229e01d60dd
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2023-11-29 10:29:13 +0100

    error don't merge

---
+++ source/Talpa/Talpa_UnitTests/OutingServiceUnittest.cs
@@ -0,0 +1,283 @@
+﻿using BusinessLogicLayer.Models;
+using BusinessLogicLayer.Services;
+
+namespace Talpa_UnitTests;
+
+public class OutingServiceUnittest
+{
+    [Test]
+    public void Create_ValidInput_Success()
+    {
+        Outing outing = new Outing
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+            OutingDates = new List<OutingDate>
+            {
+                new OutingDate
+                {
+                    Id = 2,
+                    Date = new DateTime(),
+                }
+            }
+    };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        suggestionService.Create(suggestion, userid);
+
+        Suggestion actual = repository.GetById(suggestion.Id); 
+        Assert.AreEqual(suggestion.Name, actual.Name);
+        Assert.AreEqual(suggestion.ImageUrl, actual.ImageUrl);
+    }
+
+    [Test]
+    public void Create_AlreadyExists_Failure()
+    {
+        Suggestion suggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(suggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        var actual = suggestionService.Create(suggestion, userid);
+        
+        Assert.IsNull(actual);
+    }
+
+    [Test]
+    public void Create_NullValue_Failure()
+    {
+        Suggestion suggestion = null;
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        void Create()
+        {
+            suggestionService.Create(suggestion, userid);
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(Create);
+    }
+
+    [Test]
+    public void Update_ValidInput_Success()
+    {
+        Suggestion suggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+        Suggestion newSuggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test2",
+            ImageUrl = "/url2",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(suggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        bool tryUpdate = suggestionService.Update(newSuggestion, userid);
+
+        Assert.IsTrue(tryUpdate);
+    }
+
+    [Test]
+    public void Update_DoesNotExist_Failure()
+    {
+        Suggestion suggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        void Update()
+        {
+            suggestionService.Update(suggestion, userid);
+        }
+
+        Assert.Throws<ArgumentNullException>(Update);
+    }
+
+    [Test]
+    public void Update_NullValue_Failure()
+    {
+        Suggestion suggestion = null;
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        void Update()
+        {
+            suggestionService.Update(suggestion, userid);
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(Update);
+    }
+
+    [Test]
+    public void Update_UserIDIsDifferent_Failure()
+    {
+        Suggestion suggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(suggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        bool tryUpdate = suggestionService.Update(suggestion, "user2");
+
+        Assert.IsFalse(tryUpdate);
+    }
+
+    [Test]
+    public void Delete_ValidInput_Success()
+    {
+        Suggestion suggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(suggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        bool tryDelete = suggestionService.Delete(suggestion.Id, userid);
+
+        Assert.IsTrue(tryDelete);
+    }
+
+    [Test]
+    public void Delete_DoesNotExist_Success()
+    {
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        void Delete()
+        {
+            suggestionService.Delete(643746, userid);
+        }
+
+        Assert.Throws<ArgumentNullException>(Delete);
+    }
+
+    [Test]
+    public void Delete_UserIDIsDifferent_Failure()
+    {
+        Suggestion suggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(suggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        bool tryDelete = suggestionService.Delete(suggestion.Id, "user2");
+
+        Assert.IsFalse(tryDelete);
+    }
+
+
+    [Test]
+    public void GetById_ValidInput_Success()
+    {
+        Suggestion newSuggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(newSuggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        Suggestion suggestion = suggestionService.GetById(newSuggestion.Id, userid);
+
+        Assert.AreEqual(newSuggestion.Name, suggestion.Name);
+        Assert.AreEqual(newSuggestion.ImageUrl, suggestion.ImageUrl);
+    }
+
+    [Test]
+    public void GetById_DoesNotExist_Success()
+    {
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        void GetById()
+        {
+            suggestionService.GetById(643746, userid);
+        }
+
+        Assert.Throws<ArgumentNullException>(GetById);
+    }
+
+    [Test]
+    public void GetById_UserIDIsDifferent_Failure()
+    {
+        Suggestion newSuggestion = new Suggestion
+        {
+            Id = 1,
+            Name = "Test",
+            ImageUrl = "/url",
+        };
+
+        string userid = "user1";
+
+        SuggestionTestRepository repository = new SuggestionTestRepository();
+        repository.Add(newSuggestion, userid);
+        SuggestionService suggestionService = new SuggestionService(repository);
+
+        Suggestion suggestion = suggestionService.GetById(newSuggestion.Id, "user2");
+
+        Assert.IsNull(suggestion);
+    }
+}
---
+++ source/Talpa/Talpa_UnitTests/OutingTestRepository.cs
@@ -0,0 +1,113 @@
+﻿using BusinessLogicLayer.Exceptions;
+using BusinessLogicLayer.Interfaces.Repositories;
+using BusinessLogicLayer.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Talpa_UnitTests;
+
+public class OutingTestRepository : IOutingRepository
+{
+    private readonly List<Outing> outings = new List<Outing>();
+
+    public void Add(Outing outing, int teamId)
+    {
+        outing.TeamId = teamId;
+
+        outings.Add(outing);
+    }
+
+    public bool ConfirmOuting(int id, int suggestionId, int outingDateId)
+    {
+        Outing? outing = outings.FirstOrDefault(o => o.Id == id);
+
+        outing.ConfirmedOutingDateId = outingDateId;
+        outing.ConfirmedSuggestionId = suggestionId;
+
+        return true;
+    }
+
+    public Outing Create(Outing outing, int teamId)
+    {
+        outing.TeamId = teamId;
+        
+        outings.Add(outing);
+
+        return outing;
+    }
+
+    public bool Delete(int id)
+    {
+        Outing? outing = outings.FirstOrDefault(o => o.Id == id);
+
+        outings.Remove(outing);
+
+        return true;
+    }
+    public Outing? GetById(int id)
+    {
+        return outings?
+            .FirstOrDefault(o => o.Id == id);
+    }
+    public bool Update(Outing outing)
+    {
+        Outing? outingDb = outings
+        .FirstOrDefault(o => o.Id == outing.Id);
+
+        outingDb.Name = outing.Name;
+        outingDb.Suggestions = outing.Suggestions;
+        outingDb.OutingDates = outing.OutingDates;
+        outingDb.DeadLine = outing.DeadLine;
+        outingDb.ImageUrl = outing.ImageUrl;
+
+        return true;
+    }
+
+    public List<Outing> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Outing> GetAllComplete()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Outing> GetAllCompleteFromTeam(int teamId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Outing> GetAllConfirmedFromTeam(int teamId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Outing> GetAllFromTeam(int teamId)
+    {
+        throw new NotImplementedException();
+    }
+    public Outing? GetByIdWithVotes(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Outing? GetOutingByIdWithMostVotedDatesAndSuggestions(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UserHasVotedDatesForOuting(string userId, int outingId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UserHasVotedSuggestionForOuting(string userId, int outingId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Vote(string userId, int outingId, int suggestionId, List<int> votedDateIds)
+    {
+        throw new NotImplementedException();
+    }
+}
