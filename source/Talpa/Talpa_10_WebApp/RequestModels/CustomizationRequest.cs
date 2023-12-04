a1ffb5b5df1212f26dafebe979fe10e29d5a6bd7
DeanMichiels <PS210022@edu.summacollege.nl>
2023-12-04 13:23:05 +0100

    Channge Inline styling to CSS variables

---
+++ source/Talpa/Talpa_10_WebApp/RequestModels/CustomizationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Talpa_10_WebApp.RequestModels
+{
+    public class CustomizationRequest
+    {
+        public string Primary { get; set; }
+        public string Secondary { get; set; }
+        public string Background { get; set; }
+    }
+}
