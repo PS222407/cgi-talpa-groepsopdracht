f001636b77542b38c8a9dbfd76a68424d8281a79
Jens Ramakers <jensramakers@gmail.com>
2023-12-05 13:15:18 +0100

    fixed bat file

--- source/run_project_in_testing_env.bat
+++ source/run_project_in_testing_env.bat
@@ -3,6 +3,7 @@ cd .\Talpa\Talpa_10_WebApp\
 copy appsettings.Development.json appsettings.Development.backup.json
 copy appsettings.Testing.json appsettings.Development.json
 
+dotnet build
 start "Talpa" dotnet run
 cd ..
 
@@ -12,7 +13,7 @@ cd ..
 
 timeout /nobreak /t 15
 
-dotnet test --no-build --verbosity normal ./Talpa_UITests
+dotnet test --verbosity normal ./Talpa_UITests
 
 cd .\Talpa_10_WebApp\
 
